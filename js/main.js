fbb609719f721d091a2f43f7e825fa5f321ca55c
TinySaints <37026382+TinySaints@users.noreply.github.com>
2021-03-01 18:43:34 -0700

    first example completed

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
-// js code to trigger the button goes here
+// Step 1: query the document and select the element the user will click on to make the nav show and hide 
+const button = document.querySelector('.hamburger-btn');
+
+// Step 2: place a click event on the button
+button.addEventListener('click', () => {
+    // when the button is clicked, grab the nav element and add the special class 
+    document.querySelector('nav').classList.toggle('show-nav');
+});
+
+// note: when placing a class name inside the classList medtho, do NOT include the period. it will expect a class name
